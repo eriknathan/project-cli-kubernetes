1cb64b1c2a06fec4e269c7ba77f1b7d9abbd7171
eriknathan <eriknathan.contato@gmail.com>
2025-05-02 11:36:38 -0300

    extraindo imagens de um pod

---
+++ kubenetes-get-images.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from kubernetes import client, config
+
+
+config.load_kube_config()
+v1 = client.CoreV1Api()
+
+def get_pods_all_namespaces() -> list:
+    return v1.list_pod_for_all_namespaces().items
+
+
+def get_images_from_pod(pod) -> list:
+    images = []
+    for container in pod.spec.containers:
+        images.append(container.image)
+    return images
+
+
+def main():
+    for pod in get_pods_all_namespaces():
+        print(f"name: {pod.metadata.name} | image: {get_images_from_pod(pod)}")
+
+
+if __name__ == '__main__':
+    main()
