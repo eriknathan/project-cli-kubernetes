668cfb45fe71bb88caf066ee535a53a004f6d6ff
eriknathan <eriknathan.contato@gmail.com>
2025-05-02 12:39:48 -0300

    parametrizando a cli

--- kubenetes-get-images.py
+++ kubenetes-get-images.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse
+
 from kubernetes import client, config
 from kubernetes.client import V1Pod
 from tabulate import tabulate
@@ -7,6 +9,11 @@ from colorama import  Fore, Back, Style
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
+parser = argparse.ArgumentParser(
+    description="Filter pods and their respective images from all namespaces or Kubernetes",
+    prog="kubernetes-get-images.py"
+)
+parser.add_argument("-n", "--namespace", help="Filter pods and images from a specific namespace")
 
 TABLE_HEADERS = [
     f"{Fore.CYAN}{Style.BRIGHT}Pods{Style.RESET_ALL}",
@@ -15,6 +22,10 @@ TABLE_HEADERS = [
 
 def get_pods_all_namespaces() -> list:
     return v1.list_pod_for_all_namespaces().items
+
+
+def get_pods_from_namespace(namespace: str) -> list:
+    return v1.list_namespaced_pod(namespace).items
 
 
 def get_images_from_pod(pod: V1Pod) -> list:
@@ -38,8 +49,14 @@ def str_to_magenta(text: str) -> str:
 
 
 def main():
+    args = parser.parse_args()
     table = []
-    for pod in get_pods_all_namespaces():
+    if args.namespace is None:
+        pods = get_pods_all_namespaces()
+    else:
+        pods = get_pods_from_namespace(args.namespace)
+
+    for pod in pods:
         images = list_to_coma_string(get_images_from_pod(pod))
         add_to_table(
             str_to_cyan(pod.metadata.name),
