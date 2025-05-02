07fb83c8d078dc883945f65815779b17fed0b9c8
eriknathan <eriknathan.contato@gmail.com>
2025-05-02 12:08:11 -0300

    estruturando saída em formato de tabela

--- kubenetes-get-images.py
+++ kubenetes-get-images.py
@@ -2,9 +2,12 @@
 
 from kubernetes import client, config
 from kubernetes.client import V1Pod
+from tabulate import tabulate
 
 config.load_kube_config()
 v1 = client.CoreV1Api()
+
+TABLE_HEADERS = ["Pods", "Images"]
 
 def get_pods_all_namespaces() -> list:
     return v1.list_pod_for_all_namespaces().items
@@ -14,9 +17,16 @@ def get_images_from_pod(pod: V1Pod) -> list:
     return [ container.image for container in pod.spec.containers ]
 
 
+def add_to_table(pod: V1Pod, images: list, table: list):
+    table.append([pod.metadata.name, images])
+
+
 def main():
+    table = []
     for pod in get_pods_all_namespaces():
-        print(f"name: {pod.metadata.name} | image: {get_images_from_pod(pod)}")
+        images = get_images_from_pod(pod)
+        add_to_table(pod, images, table)
+    print(tabulate(table, headers=TABLE_HEADERS, tablefmt='grid'))
 
 
 if __name__ == '__main__':
